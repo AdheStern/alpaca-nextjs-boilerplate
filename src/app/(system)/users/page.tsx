f1b60f9325fd212560e99dc3cc26b45ca94e14fa
Adhemar Claure <adhemarangelclaureflores@gmail.com>
2025-12-22 10:35:50 -0400

    feat: sidebar and header

---
+++ src/app/(system)/users/page.tsx
@@ -0,0 +1,5 @@
+// src/app/(system)/users/page.tsx
+
+export default function UsersPage() {
+  return <div className="flex flex-col gap-4 relative"></div>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,11 +31,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${audiowide.variable} antialiased`}
       >
-        <ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
           <Toaster />
         </ThemeProvider>
